d0ca97a6932944e5193680009167c73b46a8ad99
sundayred <syang.sy@protonmaill.com>
2026-05-05 22:58:40 -0700

    mass change

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Lab 5 - Starter
 Sean Yang
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+I would not use a unit test to test the “message” feature of a messaging application, because per the con of unit testing, you "Cannot test how these individual components interact with each other on an application/feature level." 
+
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+I would Would you use a unit test to test the “max message length” feature of a messaging application. Becuase, we are "Debugging on a small scale without many moving parts.." This can be executed quickly, and is testing an individual part of code in "encapsulated units", to see that the code is working properly. 
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,49 @@
-// explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const speech = window.speechSynthesis;
+  const voiceOption = document.getElementById('voice-select');
+
+  const fillOptions = () => {
+    const voices = speech.getVoices();
+
+    voiceOption.innerHTML = '<option value="select" disabled selected>Select Voice:</option>';
+
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement('option');
+      option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+      option.value = voices[i].name;
+
+      voiceOption.appendChild(option);
+    } 
+  };
+
+  fillOptions();
+  speech.onvoiceschanged = fillOptions;
+
+  const voiceButton = document.querySelector('button');
+  const textInput = document.getElementById('text-to-speak');
+  const emoji = document.querySelector('img');
+
+  voiceButton.addEventListener('click', () => {
+    const utterance = new SpeechSynthesisUtterance(textInput.value);
+    const voices = speech.getVoices();
+    const voiceName = voiceOption.value;
+
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === voiceName) {
+        utterance.voice = voices[i];
+      }
+    }
+
+    utterance.onstart = () => {
+      emoji.src = 'assets/images/smiling-open.png';
+    };
+
+    utterance.onend = () => {
+      emoji.src = 'assets/images/smiling.png';
+    };
+
+    speech.speak(utterance);
+  });
 }
